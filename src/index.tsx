3cd3c199092cd497ad0f238992a0a4dbb3f0fa3e
dbatarin <runo1194@gmail.com>
2023-07-26 12:09:23 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { API_URL } from './constants'
-import 'styles/index.scss'
+import './styles/index.scss'
 
 const apiClient = new ApolloClient({
   uri: API_URL,
